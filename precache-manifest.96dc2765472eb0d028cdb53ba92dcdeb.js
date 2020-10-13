4413701336176f419d90d1fc08f977515a3e4dbd
Jatin <adriano23012001@gmail.com>
2020-10-13 07:24:11 +0530

    Updates

---
+++ precache-manifest.96dc2765472eb0d028cdb53ba92dcdeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afaf97a5c218298654052e03df03e243",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1b35abcc069d10d83bd6",
+    "url": "/my-app/static/css/2.d1fc3ea7.chunk.css"
+  },
+  {
+    "revision": "c8e684740ca292b4767e",
+    "url": "/my-app/static/css/main.3cb07df0.chunk.css"
+  },
+  {
+    "revision": "1b35abcc069d10d83bd6",
+    "url": "/my-app/static/js/2.30f24d59.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/my-app/static/js/2.30f24d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e684740ca292b4767e",
+    "url": "/my-app/static/js/main.28fd9abb.chunk.js"
+  },
+  {
+    "revision": "e67d6344e20b0c4dbdc6",
+    "url": "/my-app/static/js/runtime-main.c824e317.js"
+  }
+]);
